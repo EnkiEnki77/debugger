1aa1e4ba8d440b8d03e04afcc5c6b68fdca59da1
Enki Layman <layman212@gmail.com>
2025-03-05 06:21:42 -0500

    Learned how to step through a loop to visualize the state for each iteration.

--- src/debugging.py
+++ src/debugging.py
@@ -1,8 +1,13 @@
-print("Statement 1")
-print("Statement 2")
-print("Statement 3")
-print("Statement 4")
-print("Statement 5")
-print("Statement 6")
-print("Statement 7")
-print("Statement 8")
+# print("Statement 1")
+# print("Statement 2")
+# print("Statement 3")
+# print("Statement 4")
+# print("Statement 5")
+# print("Statement 6")
+# print("Statement 7")
+# print("Statement 8")
+
+x = []
+for i in range(1, 6):
+    x.append(i * 3)
+print(x)
